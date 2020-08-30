a409e2b78677dd5be8b72e3dc68f6aa9d8768142
Jeff Gough <j@monad.is>
2020-08-30 18:35:03 +0100

    Create hx711 driver struct and init code

--- main.c
+++ main.c
@@ -1,55 +1,119 @@
 #include <gpiod.h>
 #include <stdio.h>
 #include <unistd.h>
-
+#include <stdint.h>
 #ifndef	CONSUMER
 #define	CONSUMER	"Consumer"
 #endif
 
+typedef struct hx711_handle_t 
+{
+	struct gpiod_chip *chip;
+	struct gpiod_line *pd_sck;
+	struct gpiod_line *dout;
+	char* chipname;
+	int32_t gain;
+	uint32_t offset;
+	uint32_t scale;
+} hx711_handle_t;
+
+hx711_handle_t* hx711_init(uint32_t gpio_pd_sck, uint32_t gpio_dout) 
+{
+	int32_t req;
+
+	hx711_handle_t* handle = malloc(sizeof(hx711_handle_t));
+	if (handle == NULL){
+		perror("malloc failed");
+		goto end;
+	}
+
+	handle->chip = gpiod_chip_open_by_name(handle->chipname);
+	if (!handle->chip) {
+		perror("Open chip failed\n");
+		goto end;
+	}
+
+	handle->pd_sck = gpiod_chip_get_line(handle->chip, gpio_pd_sck);
+	if (handle->pd_sck) {
+		perror("Get clock line failed\n");
+		goto close_chip;
+	}
+
+	handle->dout = gpiod_chip_get_line(handle->chip, gpio_dout);
+	if (handle->dout) {
+		perror("Get data line failed\n");
+		goto close_chip;
+	}
+
+	req = gpiod_line_request_output(handle->pd_sck, CONSUMER, 0);
+	if (req < 0) {
+		perror("Request clock line as output failed\n");
+		goto release_line;
+		}
+	req = gpiod_line_request_input(handle->dout, CONSUMER);
+	if (req < 0) {
+		perror("Request data line as input failed\n");
+		goto release_line;
+		}
+
+	//set defaults
+	handle->gain = 1; // set default gain to 128
+	handle->offset = 0;
+	handle->scale = 1;	
+	
+	return handle;
+
+release_line:
+	gpiod_line_release(handle->pd_sck);
+	gpiod_line_release(handle->dout);
+close_chip:
+	gpiod_chip_close(handle->chip);
+end:
+	return NULL;
+}
+
+void hx711_deinit(hx711_handle_t* hx)
+{
+	gpiod_line_release(hx->pd_sck);
+	gpiod_line_release(hx->dout);
+	gpiod_chip_close(hx->chip);
+	free(hx);
+}
+
+
 int main(int argc, char **argv)
 {
 	char *chipname = "gpiochip0";
-	unsigned int line_num = 23;	// GPIO Pin #23
+	unsigned int line_num = 7;	// GPIO Pin number 
 	unsigned int val;
 	struct gpiod_chip *chip;
 	struct gpiod_line *line;
 	int i, ret;
 
-	chip = gpiod_chip_open_by_name(chipname);
-	if (!chip) {
-		perror("Open chip failed\n");
-		goto end;
-	}
+	hx711_handle_t* scale0 = hx711_init(4, 14);
+	scale0->chipname = chipname;
+	hx711_handle_t* scale1 = hx711_init(15, 17);
+	scale1->gain = 9000;
+	
+	printf("%s\n", scale0->chipname);
+	printf("%d\n", scale1->gain);
+	
+	hx711_deinit(scale0);
+	hx711_deinit(scale1);
 
-	line = gpiod_chip_get_line(chip, line_num);
-	if (!line) {
-		perror("Get line failed\n");
-		goto close_chip;
-	}
-
-	ret = gpiod_line_request_output(line, CONSUMER, 0);
-	if (ret < 0) {
-		perror("Request line as output failed\n");
-		goto release_line;
-	}
+	return 0;
 
 	/* Blink 20 times */
-	val = 0;
-	for (i = 20; i > 0; i--) {
+/*	val = 0;
+	for (;;) {
 		ret = gpiod_line_set_value(line, val);
 		if (ret < 0) {
 			perror("Set line output failed\n");
 			goto release_line;
 		}
-		printf("Output %u on line #%u\n", val, line_num);
-		sleep(1);
+		//printf("Output %u on line #%u\n", val, line_num);
+		//sleep(1);
 		val = !val;
 	}
-
-release_line:
-	gpiod_line_release(line);
-close_chip:
-	gpiod_chip_close(chip);
-end:
-	return 0;
+*/
 }
