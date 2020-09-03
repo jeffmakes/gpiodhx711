227f81668a948662f6be786a07bdb5260f9b4482
Jeff Gough <j@monad.is>
2020-09-03 19:18:10 +0100

    Add second scale instance. Works a treat with no debugging

--- main.c
+++ main.c
@@ -189,14 +189,17 @@ int main(int argc, char **argv)
 
 
 	hx711_handle_t* scale0 = hx711_init(4, 14);
-	//hx711_handle_t* scale1 = hx711_init(15, 17);
+	hx711_handle_t* scale1 = hx711_init(15, 17);
 	
 	printf("%d\n", hx711_read_average(scale0));
+	printf("%d\n", hx711_read_average(scale1));
 	hx711_tare(scale0);
+	hx711_tare(scale1);
 	hx711_set_scale(scale0, 0.0000184267); //assuming Jeff as 100kg proof mass
+	hx711_set_scale(scale1, 0.0000184267); //assuming Jeff as 100kg proof mass
 	for (i = 0; i<600; i++)
 	{
-		printf("%d %f\n", i, hx711_read_kg(scale0));
+		printf("%d\t%f\t%f\n", i, hx711_read_kg(scale0),hx711_read_kg(scale1) );
 		sleep(0.1);
 	}
 	
