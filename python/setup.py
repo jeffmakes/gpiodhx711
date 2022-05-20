2a88a37d0ab8f29f9d4cc44138c0eee183175e20
Jeff Gough <j@jeffreygough.com>
2022-05-20 20:20:10 +0100

    CFFI builder and module for the Python bindings

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="gpiodhx711",
+      version="0.1",
+      description="Fast driver for the HX711 load cell amplifier based on the Kernel gpiod interface, including Python bindings",
+      author="Jeff Gough",
+      py_modules=["gpiodhx711"],
+      setup_requires=["cffi"],
+      cffi_modules=["build_gpiodhx711.py:ffibuilder"],
+      install_requires=["cffi"],
+)
