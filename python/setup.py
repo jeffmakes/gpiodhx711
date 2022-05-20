aa23200698dbcd29f3b824767ded1e26c1fdfdb3
Jeff Gough <j@jeffreygough.com>
2022-05-20 20:30:55 +0100

    Correct dependency for the path module

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ setup(name="gpiodhx711",
       description="Fast driver for the HX711 load cell amplifier based on the Kernel gpiod interface, including Python bindings",
       author="Jeff Gough",
       py_modules=["gpiodhx711"],
-      setup_requires=["cffi"],
+      setup_requires=["cffi", "path"],
       cffi_modules=["build_gpiodhx711.py:ffibuilder"],
       install_requires=["cffi"],
 )
