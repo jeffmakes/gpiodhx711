8267ec29043a63245e320f1fdd5cc73b0be39e5f
Jeff Gough <j@jeffreygough.com>
2022-05-22 20:29:01 +0100

    Add Python functions corresponding to power, scaling, taring and reading. Test code now works on one scale

--- python/gpiodhx711.py
+++ python/gpiodhx711.py
@@ -2,16 +2,37 @@ from _gpiodhx711 import lib, ffi
 
 
 class GpiodHx711:
-    def __init__(self, gpio_sck, gpio_dout):
-        self._hx = lib.hx711_init(self.gpio_sck, self.gpio_dout)
+    def __init__(self, gpio_sck, gpio_dout, gpio_power_en):
+        self._hx = lib.hx711_init(gpio_sck, gpio_dout, gpio_power_en)
     
     def __del__(self):
         lib.hx711_deinit(self._hx)
 
+    def set_power(self, value):
+        lib.hx711_set_power(self._hx, value)
+
+    def tare(self):
+        lib.hx711_tare(self._hx)
+
+    def set_scale(self, scale):
+        lib.hx711_set_scale(self._hx, scale)
+
+    def read_kg(self):
+        return lib.hx711_read_kg(self._hx)
+
 
 def main():
-    scale0 = GpiodHx711(4, 14)
-    
+    scale0 = GpiodHx711(4, 14, 12)
+    scale0.set_power(True)
+
+    scale0.tare()
+    scale0.set_scale(0.0000184267) #assuming Jeff as 100kg proof mass
+
+    for i in range (500):
+        kg = scale0.read_kg()
+        print(kg)
+
+
 if __name__ == "__main__":
     main()
 
